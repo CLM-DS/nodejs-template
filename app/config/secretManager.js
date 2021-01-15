7ac850100f5b4bf9a1124009b3552b44309a826a
Yanny Hernández <yan.hernan.551@gmail.com>
2021-01-15 17:25:39 +0000

    perfomance in managed from secret manager

--- app/config/secretManager.js
+++ app/config/secretManager.js
@@ -1,13 +1,15 @@
 const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
-
+​
 /**
  * Load Secret and return object to values access
  * @param {*} options params to search data in GCP
  * @param {'online'|'offline'} mode online: used secret manager GCP, offline: enviroment vaiables
  */
 const loadSecrets = async (
-  options = { version: 'latest', env: 'dev', project: '' },
-  mode = 'online',
+  options = {
+    version: 'latest', env: 'qa', project: '', keys: [],
+  },
+  mode = 'offline',
 ) => {
   let secrets = {};
   if (mode === 'online') {
@@ -15,20 +17,31 @@ const loadSecrets = async (
     const [listSecrets] = await client.listSecrets({
       parent: `projects/${options.project}`,
     });
-    listSecrets.forEach(async (secret) => {
+    // eslint-disable-next-line no-plusplus
+    for (let index = 0; index < listSecrets.length; index++ ) {
+      const secret = listSecrets[index];
       const name = secret.name.toString();
-      const [version] = await client.accessSecretVersion({
-        name: `${name}/versions/${options.version}`,
-      });
       let env = options.env.toUpperCase();
       if (!env.startsWith('_')) {
         env = `_${env}`;
       }
       if (name.endsWith(env)) {
         const key = name.substring(0, name.length - env.length).toUpperCase();
-        secrets[key] = version.payload;
+        const keywordSecret = 'SECRETS/';
+        const subNameIndex = key.indexOf(keywordSecret) + keywordSecret.length;
+        const keyName = key.substring(subNameIndex);
+        if (options.keys.length > 0 && options.keys.indexOf(keyName) < 0) {
+          // eslint-disable-next-line no-continue
+          continue;
+        }
+        // eslint-disable-next-line no-await-in-loop
+        const [version] = await client.accessSecretVersion({
+          name: `${name}/versions/${options.version}`,
+        });
+        const value = version.payload.data.toString();
+        secrets[keyName] = value;
       }
-    });
+    }
   } else {
     secrets = process.env;
   }
@@ -36,7 +49,7 @@ const loadSecrets = async (
     get: (name) => secrets[name.toUpperCase()],
   };
 };
-
+​
 module.exports = {
   loadSecrets,
 };
