30752d0034e1bbd93dfcd1cfa692e11abe6c3830
Sulpick Barbella <ext_sbarbellag@tottus.cl>
2021-06-24 10:42:50 -0500

    Added mongo migrations to template

---
+++ app/migrations/20210624153837-template.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+module.exports = {
+  async up(db, client) {
+    // TODO write your migration here.
+  },
+
+  async down(db, client) {
+    // TODO write the statements to rollback your migration (if possible)
+  },
+};
